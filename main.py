55a15383b0480dff2451223ea5b1c38cd8de32e7
George <georgessidhom38@gmail.com>
2024-04-17 23:20:46 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+print(sys.version)
+
+print('Roses are red, violets are blue, I thought voldy was evil until I met you (Dolores Umbridge)') 
+x = 1
+y = 0.5
+z = 2
+
+if 2 * y == x:
+    print('2 * 0.5 = 1')
+
+if 2 * x == z:
+    print('2 * 1 = 2')
+
+if 4 * y == z:
+    print('4 * 0.5 = 2')
+
+"""
+This is a comment
+"""
